b5673a845fa456ada04fa3ce33abf6bd7e241fe2
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2026-04-14 20:26:20 +0530

    ...

--- 018.Heart/app.js
+++ 018.Heart/app.js
@@ -2,11 +2,13 @@ const grayHeart = document.querySelector(".gray-heart");
 const redHeart = document.querySelector(".red-heart");
 
 grayHeart.addEventListener("click", () => {
-  redHeart.classList.add("animation");
-  grayHeart.classList.add("fill-color");
-});
+    redHeart.classList.add("animation");
+    grayHeart.classList.add("fill-color");
+  }
+);
 
 redHeart.addEventListener("click", () => {
-  redHeart.classList.remove("animation");
-  grayHeart.classList.remove("fill-color");
-});
+    redHeart.classList.remove("animation");
+    grayHeart.classList.remove("fill-color");
+  }
+);
